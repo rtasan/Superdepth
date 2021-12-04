d8ae0bec0586c8d5fa10099c33cfe7a60a184121
rta_28 <rtasansax@gmail.com>
2021-12-05 00:11:10 +0900

    add: logger

---
+++ src/Superdepth/IO/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Superdepth.IO
+{
+    internal class Logger
+    {
+        public static TextBox logTextBox;
+
+        public static void Log(string message)
+        {
+            logTextBox.AppendText(message + "\n");
+            logTextBox.ScrollToEnd();
+        }
+    }
+}
